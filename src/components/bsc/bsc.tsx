5c414e039a68f101f9fc1ebc52bd032581eb4384
Mirco <mirco.seccardi@gmail.com>
2022-04-02 11:27:24 +0200

    flex handled in react

--- src/components/bsc/bsc.tsx
+++ src/components/bsc/bsc.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
+import { Flex } from '@adobe/react-spectrum';
 
 class Bsc extends React.Component {
   render() {
     return (
-      <div>
+      <Flex direction="row" justifyContent="space-around" gap="size-100">
         <div>HEre my bsc</div>
         <div>HEre my bsc</div>
-        <div>HEre my bsc</div>
-        <div>HEre my bsc</div>
-        <div>HEre my bsc</div>
-        <div>HEre my bsc</div>
-        <div>HEre my bsc</div>
-      </div>
+      </Flex>
     );
   }
 }
