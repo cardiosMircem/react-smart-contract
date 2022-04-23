08a0d04725d34b4f5bb64e5ad85c4130bff01bc5
mirco.seccardi <mirco.seccardi@gmail.com>
2022-04-23 10:54:27 +0200

    work in progress for using hello world

--- src/components/bsc/bsc.tsx
+++ src/components/bsc/bsc.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { Flex } from '@adobe/react-spectrum';
+import Web3 from 'web3';
+import {AbiItem} from 'web3-utils';
 
-class Bsc extends React.Component {
+
+class Bsc extends React.Component { 
+  
+  // testnet
+  activateBSCSmartContract() {
+
+  const web3 = new Web3();
+  // .setProvider('https://data-seed-prebsc-1-s1.binance.org:8545')
+
+  
+  const contractAddress = '0xBa6fefeB324e31a5B976E69E794Ae15244df2D21';
+  const abi: AbiItem =
+    {
+      inputs:[],
+      name:"sayHello",
+      outputs:[
+        {
+          internalType:"string",
+          name:"",type:"string"
+        }
+      ],
+      stateMutability:"pure",
+      type:'function'
+    };
+  
+  const contract = new web3.eth.Contract(abi, contractAddress);
+
+
+  // console.log(contract.deploy());
+  }
+
   render() {
     return (
       <Flex direction="row" justifyContent="space-around" gap="size-100">
         <div>HEre my bsc</div>
         <div>HEre my bsc</div>
+        <button onClick={() => this.activateBSCSmartContract()}>click here to activate smart contract</button>
       </Flex>
     );
   }
