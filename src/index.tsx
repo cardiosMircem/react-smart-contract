551195a1246a7b731d4b27a30649babe493bb11e
mirco.seccardi <mirco.seccardi@gmail.com>
2022-07-10 12:05:26 +0200

    from 17 to 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (container === null) {
+  throw new Error('Root container missing in index.html');
+}
+
+// Create a root.
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/layout/react-route.tsx
+++ src/layout/react-route.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Bsc from '../components/bsc/bsc';
 import Home from '../components/home/home';
 
