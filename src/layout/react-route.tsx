4abaf63d099523c58bb53851f046f883306cf680
mirco.seccardi <mirco.seccardi@gmail.com>
2022-07-10 12:23:17 +0200

    back to browserRouter

--- src/layout/react-route.tsx
+++ src/layout/react-route.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Bsc from '../components/bsc/bsc';
 import Home from '../components/home/home';
 
 class ReactRoute extends React.Component {
   render() {
     return (
-      // change this to Browser in development
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/bsc" element={<Bsc />} />
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Home />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
