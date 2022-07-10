1afcf9fdc4b17ad8599bcbf48a6d08995f7983e5
mirco.seccardi <mirco.seccardi@gmail.com>
2022-07-10 12:50:04 +0200

    back

--- src/layout/react-route.tsx
+++ src/layout/react-route.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Bsc from '../components/bsc/bsc';
 import Home from '../components/home/home';
 
 class ReactRoute extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/react-smart-contract" element={<Home />} />
           <Route path="/react-smart-contract/bsc" element={<Bsc />} />
           <Route path="/react-smart-contract/home" element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
